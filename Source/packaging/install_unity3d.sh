4a0fcb1d444e868694910b63d3dc5065750a7e1e
Thorsten Bux <thorsten.bux@outlook.com>
2018-04-08 12:17:12 +1200

    some unity installation script fixes

--- Source/packaging/install_unity3d.sh
+++ Source/packaging/install_unity3d.sh
@@ -7,19 +7,19 @@ usage() {
 	echo "USAGE: $0 file[.dmg|.pkg] [installation path]"
 }
 
-if [ $# -eq 0 ] || [ $# -gt 2 ]; then
+if [ $# -eq 0 ] || [ $# -gt 2 ] ; then
 	echo "ERROR: invalid number of arguments"
 	usage
 	exit -1	
 fi
 
-if [ ! -z $ipath ] && [ -d $ipath ]; then
+if [ ! -z $ipath ] && [ -d $ipath ] ; then
 	echo "ERROR: $ipath directory already present"
 	usage
 	exit -1	
 fi
 
-if [ ! -f $file ]; then
+if [ ! -f $file ] ; then
 	echo "ERROR: $file not a file"
 	usage
 	exit -1
@@ -31,7 +31,7 @@ fi
 # check assumptions
 apphome=/Applications/Unity
 
-if [ -d "$apphome" ]; then
+if [ -d "$apphome" ] ; then
     echo "ERROR: $apphome already present"
     exit -1
 fi
@@ -39,10 +39,10 @@ fi
 filename=$(basename "$file")
 extension="${filename##*.}"
 
-if [ "$extension" == "dmg" ]; then
+if [ "$extension" = "dmg" ] ; then
 	echo "Detected dmg $file"
 	dmg=$file
-elif [ "$extension" == "pkg" ]; then
+elif [ "$extension" = "pkg" ] ; then
 	echo "Detected pkg $file"
 	pkg=$file
 else
@@ -50,7 +50,7 @@ else
 	exit -1
 fi
 
-if [ -n "$dmg" ]; then
+if [ -n "$dmg" ] ; then
 	hdiutil verify $dmg || exit 1
 	tempfoo=`basename $0`
 	TMPFILE=`mktemp /tmp/${tempfoo}.XXXXXX` || exit 1
@@ -62,23 +62,23 @@ fi
 installer -verbose -pkg "$pkg" -dominfo -volinfo -pkginfo
 sudo installer -pkg "$pkg" -target /
 
-if [ -n "$dmg" ]; then
+if [ -n "$dmg" ] ; then
 	hdiutil unmount "$vol"
 fi
 
-if [ ! -d "$apphome" ]; then
+if [ ! -d "$apphome" ] ; then
     echo "ERROR: $apphome not present after installation. Something went wrong"
     exit -1
 fi
 
 appversion=`/usr/libexec/PlistBuddy -c 'Print :CFBundleVersion' "$apphome"/Unity.app/Contents/Info.plist`
-if [ -d "$apphome$appversion" ]; then
+if [ -d "$apphome$appversion" ] ; then
     echo "ERROR: "$apphome$appversion" already present on disk. Something went wrong"
     sudo rm -rf "$apphome"
     exit -1
 fi
 
-if [ ! -z "$ipath" ]; then
+if [ ! -z "$ipath" ] ; then
 	ipath="$apphome$appversion"
     sudo mv "$apphome" "$ipath"
     echo "Unity $appversion installed at $ipath"
