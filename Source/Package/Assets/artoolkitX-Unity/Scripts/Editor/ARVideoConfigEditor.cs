049b3edf5735aaf72696a441fba41ea8c5fc82c4
Philip Lamb <phil.git@eden.net.nz>
2023-03-10 16:02:31 +1300

    Complete video config editor.

--- Source/Package/Assets/artoolkitX-Unity/Scripts/Editor/ARVideoConfigEditor.cs
+++ Source/Package/Assets/artoolkitX-Unity/Scripts/Editor/ARVideoConfigEditor.cs
@@ -45,17 +45,33 @@ using System;
 [CustomEditor(typeof(ARVideoConfig))]
 public class ARVideoConfigEditor : Editor
 {
+    bool[] showPlatformConfig = null;
+
     public override void OnInspectorGUI()
     {
         ARVideoConfig arvideoconfig = (ARVideoConfig)target;
         if (arvideoconfig == null) return;
+        bool needSave = false;
+
+        // Init the per-platform turndown arrows.
+        if (showPlatformConfig == null)
+        {
+            showPlatformConfig = new bool[arvideoconfig.configs.Count];
+            for (int i = 0; i < arvideoconfig.configs.Count; i++)
+            {
+                if (arvideoconfig.configs[i].platform == Application.platform) {
+                    showPlatformConfig[i] = true;
+                }
+            }
+        }
 
         for (int pci = 0; pci < arvideoconfig.configs.Count; pci++)
         {
             // Copy the config entry from the list.
             ARVideoConfig.ARVideoPlatformConfig pc = arvideoconfig.configs[pci];
 
-            EditorGUILayout.LabelField(pc.name);
+            showPlatformConfig[pci] = EditorGUILayout.Foldout(showPlatformConfig[pci], pc.name);
+            if (!showPlatformConfig[pci]) continue;
 
             EditorGUI.BeginDisabledGroup(pc.isUsingManualConfig);
 
@@ -73,7 +89,12 @@ public class ARVideoConfigEditor : Editor
                 }
                 return true;
             };
-            pc.inputSelectionMethod = (ARVideoConfig.ARVideoConfigInputSelectionMethod)EditorGUILayout.EnumPopup(new GUIContent("Select input by"), pc.inputSelectionMethod, checkSelectionMethod, false);
+            ARVideoConfig.ARVideoConfigInputSelectionMethod tempIsm = (ARVideoConfig.ARVideoConfigInputSelectionMethod)EditorGUILayout.EnumPopup(new GUIContent("Select input by"), pc.inputSelectionMethod, checkSelectionMethod, false);
+            if (tempIsm != pc.inputSelectionMethod)
+            {
+                pc.inputSelectionMethod = tempIsm;
+                needSave = true;
+            }
 
             // If we should put up a source info list, and we're on that platform, do it.
             if (pc.inputSelectionMethod == ARVideoConfig.ARVideoConfigInputSelectionMethod.VideoSourceInfoList && pc.platform == Application.platform)
@@ -91,7 +112,11 @@ public class ARVideoConfigEditor : Editor
                     int i = openTokens.IndexOf(pc.VideoSourceInfoListOpenToken);
                     i = i < 0 ? 0 : i; // If current option not found (i == -1), revert to first option in list.
                     i = EditorGUILayout.Popup("Video source:", i, arvideoconfig.sourceInfoList.Select(si => $"{si.name} ({si.model})").ToArray());
-                    pc.VideoSourceInfoListOpenToken = arvideoconfig.sourceInfoList[i].open_token;
+                    if (pc.VideoSourceInfoListOpenToken != arvideoconfig.sourceInfoList[i].open_token)
+                    {
+                        pc.VideoSourceInfoListOpenToken = arvideoconfig.sourceInfoList[i].open_token;
+                        needSave = true;
+                    }
                 }
             }
             if (pc.inputSelectionMethod == ARVideoConfig.ARVideoConfigInputSelectionMethod.CameraAtPosition || pc.inputSelectionMethod == ARVideoConfig.ARVideoConfigInputSelectionMethod.NthCameraAtPosition)
@@ -103,31 +128,92 @@ public class ARVideoConfigEditor : Editor
                     else if (p == ARController.AR_VIDEO_POSITION.AR_VIDEO_POSITION_OTHER && pc.platform == RuntimePlatform.Android) return true;
                     return false;
                 };
-                pc.position = (ARController.AR_VIDEO_POSITION)EditorGUILayout.EnumPopup(new GUIContent("Camera position"), pc.position, checkPosition, false);
+                ARController.AR_VIDEO_POSITION tempPos = (ARController.AR_VIDEO_POSITION)EditorGUILayout.EnumPopup(new GUIContent("Camera position"), pc.position, checkPosition, false);
+                if (tempPos != pc.position)
+                {
+                    pc.position = tempPos;
+                    needSave = true;
+                }
             }
             if (pc.inputSelectionMethod == ARVideoConfig.ARVideoConfigInputSelectionMethod.NthCamera || pc.inputSelectionMethod == ARVideoConfig.ARVideoConfigInputSelectionMethod.NthCameraAtPosition)
             {
-                pc.index = EditorGUILayout.IntField("Camera number", pc.index + 1) - 1;
+                int tempIndex = EditorGUILayout.IntField("Camera number", pc.index + 1) - 1;
+                if (tempIndex != pc.index)
+                {
+                    pc.index = tempIndex;
+                    needSave = true;
+                }
             }
+
+            switch (ARVideoConfig.modules[pc.module].sizeSelectionStrategy)
+            {
+                case ARVideoConfig.ARVideoSizeSelectionStrategy.AVFoundationPreset:
+                    var presets = ARVideoConfig.AVFoundationPresets.Where(kv => pc.platform == RuntimePlatform.IPhonePlayer ? kv.Value.availableiOS : kv.Value.availableMac).ToList();
+                    int pi = presets.FindIndex(p => p.Key == pc.AVFoundationPreset);
+                    pi = pi < 0 ? 0 : pi;
+                    int tempPresetIndex = EditorGUILayout.Popup("Size preset", pi, presets.Select(p => p.Value.description).ToArray());
+                    if (tempPresetIndex != pi)
+                    {
+                        pc.AVFoundationPreset = presets[tempPresetIndex].Key;
+                        needSave = true;
+                    }
+                    break;
+                case ARVideoConfig.ARVideoSizeSelectionStrategy.SizePreference:
+                    var keys = ARVideoConfig.SizePreferences.Keys.ToList();
+                    int spi = keys.FindIndex(p => p == pc.sizePreference);
+                    spi = spi < 0 ? 0 : spi;
+                    int tempSizePrefIndex = EditorGUILayout.Popup("Size preference", spi, ARVideoConfig.SizePreferences.Values.Select(sp => sp.description).ToArray());
+                    if (tempSizePrefIndex != spi)
+                    {
+                        pc.sizePreference = keys[tempSizePrefIndex];
+                        needSave = true;
+                    }
+                    if (ARVideoConfig.SizePreferences[pc.sizePreference].usesWidthAndHeightFields)
+                    {
+                        goto case ARVideoConfig.ARVideoSizeSelectionStrategy.WidthAndHeight;
+                    }
+                    break;
+                case ARVideoConfig.ARVideoSizeSelectionStrategy.WidthAndHeight:
+                    EditorGUILayout.BeginHorizontal();
+                    int tempWidth = EditorGUILayout.IntField("Width", pc.width);
+                    int tempHeight = EditorGUILayout.IntField("Height", pc.height);
+                    EditorGUILayout.EndHorizontal();
+                    if (tempWidth != pc.width || tempHeight != pc.height)
+                    {
+                        pc.width = tempWidth;
+                        pc.height = tempHeight;
+                        needSave = true;
+                    }
+                    break;
+                case ARVideoConfig.ARVideoSizeSelectionStrategy.None:
+                default:
+                    break;
+            }
+
 
             EditorGUI.EndDisabledGroup();
 
-            pc.isUsingManualConfig = EditorGUILayout.Toggle("Manually override", pc.isUsingManualConfig);
-
-            EditorGUI.BeginDisabledGroup(!pc.isUsingManualConfig);
-            if (pc.isUsingManualConfig)
+            bool tempUmc = EditorGUILayout.Toggle("Manually override", pc.isUsingManualConfig);
+            if (tempUmc != pc.isUsingManualConfig)
             {
-                pc.manualConfig = EditorGUILayout.TextField("Video config.", pc.manualConfig);
+                pc.isUsingManualConfig = tempUmc;
+                needSave = true;
             }
-            else
-            {
-                EditorGUILayout.TextField("Video config.", arvideoconfig.GetVideoConfigStringForPlatform(pc.platform));
-            }
-            EditorGUI.EndDisabledGroup();
 
+            // If using manual config, allow user to edit it, otherwise show autoconfig string.
+            string tempMc = EditorGUILayout.TextField("Video config.", pc.isUsingManualConfig ? pc.manualConfig : arvideoconfig.GetVideoConfigStringForPlatform(pc.platform));
+            if (pc.isUsingManualConfig && tempMc != pc.manualConfig)
+            {
+                pc.manualConfig = tempMc;
+                needSave = true;
+            }
 
             // Save the changes back to the list.
-            arvideoconfig.configs[pci] = pc;
+            if (needSave)
+            {
+                arvideoconfig.configs[pci] = pc;
+                EditorUtility.SetDirty(target);
+            }
         } // foreaach platform
     } // OnInspectorGUI
 }
